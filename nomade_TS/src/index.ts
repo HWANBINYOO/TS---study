61fd61f27cb3461e149626e1e4eac7bc53764586
HAWNBINYOO <dngh0825@naver.com>
2022-03-06 01:50:36 +0900

    3/6/2

--- nomade_TS/src/index.ts
+++ nomade_TS/src/index.ts
@@ -1,9 +1,16 @@
+import * as CryptoJS from "crypto-js";
 class Block {
     public hash: string;
     public previousHash:string;
     public index:number;
     public data:string;
     public timestamp:number;
+
+    //블록을 생성하지 않아도 되는 mathod
+    static calculateBlockHash = (
+        index: number,previousHash: string,timestamp: number,data: string
+        ) : string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
     constructor(
         index:number,
         hash: string,
@@ -21,11 +28,14 @@ class Block {
 
 const genesisBlock:Block = new Block(0, "20202020" , "" ,"Hello", 123456);
 
-let blockchain:[Block] = [genesisBlock];
-
-blockchain.push("stuff");
+let blockchain:Block[] = [genesisBlock];
 
 console.log(blockchain);
 
+const getBlockchain = () : Block[] => blockchain;
+
+const getLatestBlock = () : Block => blockchain[blockchain.length -1];
+
+const getNewTimeStamp = () :number => Math.round(new Date().getTime() / 1000);
 
 export{};
